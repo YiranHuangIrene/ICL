e102066339d78bbdeb13b508387aa9ba2543ff54
Yiran Huang <yiranh97@gmail.com>
2025-04-17 20:35:39 +0000

    write in dataloader

--- dataset.py
+++ dataset.py
@@ -187,13 +187,15 @@ def generate_input_seqs_mm_v1(mus_label_m1, mus_class_m1, mus_label_m2, mus_clas
     K_c1 = 128  
     K_c2 = int(K_c1 * K2 / K1)
     K_c2 = K_c2 // L2 * L2
+    if K_c2 < L2:
+        K_c2 = L2
     mus_class_new1 = np.random.normal(size = (K_c1,D1))/np.sqrt(D1)
     mus_class_new2 = np.random.normal(size = (K_c2,D2))/np.sqrt(D2)
     if K_c1 < L1 or K_c1%L1 != 0:
-        print("K > L and K%L == 0 is required")
+        print("K_c1 > L1 and K_c1%L1 == 0 is required")
         return 0
     if K_c2 < L2 or K_c2%L2 != 0:
-        print("K > L and K%L == 0 is required")
+        print("K_c2 > L2 and K_c2%L2 == 0 is required")
         return 0
     labels_class_new1 =  np.tile(np.arange(L1),int(K_c1/L1))
     labels_class_new2 =  np.tile(np.arange(L2),int(K_c2/L2))
@@ -514,4 +516,243 @@ class ICLDataset(IterableDataset):
             inputs, labels = self.generate_sequence()
             # Convert to tensors and yield as a batch
             yield torch.FloatTensor(inputs), torch.FloatTensor(labels)
+            self.n_samples += 1
+
+class MMDataset(IterableDataset):
+    def __init__(self, mus_label_m1, mus_class_m1, mus_label_m2, mus_class_m2, labels_class_m2, mapping_m2_to_m1, N,S, eps1= 0.1, eps2 = 0.1, B= 0, p_B = 0, P1 = None, P2 = None, p_C = 0, flip_labels = False, output_target_labels = False, no_repeats = False, seq_labels = False, datasize = 0):
+        super().__init__()
+        self.mus_label_m1 = mus_label_m1
+        self.mus_class_m1 = mus_class_m1
+        self.mus_label_m2 = mus_label_m2
+        self.mus_class_m2 = mus_class_m2
+        self.labels_class_m2 = labels_class_m2
+        self.mapping_m2_to_m1 = mapping_m2_to_m1
+        self.N = N
+        self.S = S
+        self.eps1 = eps1
+        self.eps2 = eps2
+        self.B = B
+        self.p_B = p_B
+        self.p_C = p_C
+        self.flip_labels = flip_labels
+        self.output_target_labels = output_target_labels
+        self.no_repeats = no_repeats
+        self.seq_labels = seq_labels
+        self.datasize = datasize
+        self.n_samples = 0
+        
+        L1 = mus_label_m1.shape[0]
+        L2 = mus_label_m2.shape[0]
+        K1 = mus_class_m1.shape[0]
+        K2 = mus_class_m2.shape[0]
+        D1 = mus_class_m1.shape[1]
+        D2 = mus_class_m2.shape[1]
+        
+        self.L1 = L1
+        self.L2 = L2
+        self.K1 = K1
+        self.K2 = K2
+        self.D1 = D1
+        self.D2 = D2
+            
+        # Set up class distribution if not provided
+        if P1 is None or len(P1) != K1:
+            self.P1 = np.ones(K1)/K1
+        else:
+            self.P1 = P1    
+        if P2 is None or len(P2) != K2:
+            self.P2 = np.ones(K2)/K2
+        else:
+            self.P2 = P2
+            
+        # Set up OOD class embeddings
+        self.K_c1 = 128
+        self.K_c2 = int(self.K_c1 * K2 / K1)
+        self.K_c2 = self.K_c2 // L2 * L2
+        if self.K_c2 < L2:
+            self.K_c2 = L2
+        if self.K_c1 < L1 or self.K_c1%L1 != 0:
+            print("K_c1 > L1 and K_c1%L1 == 0 is required")
+            raise ValueError("K_c1 > L1 and K_c1%L1 == 0 is required")
+        if self.K_c2 < L2 or self.K_c2%L2 != 0:
+            print("K_c2 > L2 and K_c2%L2 == 0 is required")
+            raise ValueError("K_c2 > L2 and K_c2%L2 == 0 is required")
+        
+        # Validate burstiness parameter
+        if B > 0 and N % B != 0:
+            raise ValueError("N must be divisible by B when B > 0")
+        if B >= N:
+            raise ValueError("B must be less than N")
+        if B == 0:
+            self.B = int(N/2)
+            self.p_B = 0
+        
+        
+    def generate_sequence(self):
+        S = self.S
+        N = self.N
+        B = self.B
+        P1 = self.P1
+        P2 = self.P2
+        K1 = self.K1
+        K2 = self.K2
+        L1 = self.L1
+        L2 = self.L2
+        D1 = self.D1
+        D2 = self.D2
+        K_c1 = self.K_c1
+        K_c2 = self.K_c2
+        mus_class_m1 = self.mus_class_m1
+        mus_class_m2 = self.mus_class_m2
+        mus_label_m2 = self.mus_label_m2
+        labels_class_m2 = self.labels_class_m2
+        mapping_m2_to_m1 = self.mapping_m2_to_m1
+        p_B = self.p_B
+        p_C = self.p_C
+        eps1 = self.eps1
+        eps2 = self.eps2
+        flip_labels = self.flip_labels
+        output_target_labels = self.output_target_labels
+        no_repeats = self.no_repeats
+        seq_labels = self.seq_labels
+        
+        # Decide in final input dimension
+        inputs_mm = np.zeros((S,3*N+1,D1))
+        inputs_2 = np.zeros((S,N+1,D2))
+        
+        # Generate OOD classes for distribution 1 (modality 1, here means llm) and distribution 2 (modality 2, here means vision)
+        mus_class_new1 = np.random.normal(size = (K_c1,D1))/np.sqrt(D1)
+        mus_class_new2 = np.random.normal(size = (K_c2,D2))/np.sqrt(D2)
+        labels_class_new1 =  np.tile(np.arange(L1),int(K_c1/L1))
+        labels_class_new2 =  np.tile(np.arange(L2),int(K_c2/L2))
+        mapping_m2_to_m1_new = []
+        for k2 in range(K_c2):
+            label2 = labels_class_new2[k2]
+            matching_classes = np.where(labels_class_new1 == label2)[0]
+            mapping_m2_to_m1_new.append(matching_classes)
+        mapping_m2_to_m1_new = np.array(mapping_m2_to_m1_new)
+        # Choose the context-item classes for distribution 2, then choose the context-item classes for distribution 1 with the same labels
+        choices_2 = np.zeros((S,int(N/B)), dtype = int)
+        if no_repeats:
+            for s in range(S):
+                label_choices = np.random.choice(np.arange(L2), size = (int(N/B)), replace = False)
+                pos_choices = np.random.choice(np.arange(int(K2/L2)), size = (int(N/B)))
+                choices_2[s] = pos_choices*L2 + label_choices
+        else:
+            choices_2 = np.random.choice(np.arange(K2), size = (S,int(N/B)), p = P2)
+        choices_1 = np.zeros((S,int(N/B)), dtype = int)
+        for s in range(S):
+            choices = []
+            for k in choices_2[s]:
+                Pk=P1[mapping_m2_to_m1[k]]
+                Pk/=np.sum(Pk)
+                choices.append(np.random.choice(mapping_m2_to_m1[k], p = Pk))
+            choices_1[s] = np.array(choices)
+        # Interleave choices_1 and choices_2 into alternating pattern (x1,y1,x2,y2,...)
+        choices = np.zeros((S, 2*int(N/B)), dtype=int)
+        choices[:,0::2] = choices_1  # Even indices get choices_1 
+        choices[:,1::2] = choices_2  # Odd indices get choices_2
+
+        choices = np.tile(choices,B)
+        # Shuffle choices pairwise (keeping x,y pairs together)
+        for s in range(S):
+            # Reshape into pairs, shuffle pairs, then flatten back
+            pairs = choices[s].reshape(-1, 2)  # Shape: (N/B, 2)
+            np.random.shuffle(pairs)  # Shuffle along first dimension
+            choices[s] = pairs.flatten()  # Back to 1D array
+        choices_1 = choices[:,0::2]
+        choices_2 = choices[:,1::2]
+        
+        # Select target from distribution 2
+        targets_ind = np.random.choice(choices_2.shape[1], size = (S,))
+        targets = choices_2[np.arange(S),targets_ind]
+    
+        # Choose OOD context items classes for distribution 2 
+        choices_c2 = np.zeros((S,int(N/B)), dtype = int)
+        if no_repeats:
+            for s in range(S):
+                label_choices = np.random.choice(np.arange(L2), size = (int(N/B)), replace = False)
+                pos_choices = np.random.choice(np.arange(int(K_c2/L2)), size = (int(N/B)))
+                choices_c2[s] = pos_choices*L2 + label_choices
+        else:
+            choices_c2 = np.random.choice(np.arange(K_c2), size = (S,int(N/B)))
+        choices_c1 = np.zeros((S,int(N/B)), dtype = int)
+        for s in range(S):
+            choices_c = []
+            for k in choices_c2[s]:
+                choices_c.append(np.random.choice(mapping_m2_to_m1_new[k]))
+            choices_c1[s] = np.array(choices_c)
+        choices_c = np.zeros((S, 2*int(N/B)), dtype=int)
+        choices_c[:,0::2] = choices_c1  # Even indices get choices_c1 
+        choices_c[:,1::2] = choices_c2  # Odd indices get choices_c2
+        choices_c = np.tile(choices_c,B)
+        # Shuffle choices pairwise (keeping x,y pairs together)
+        for s in range(S):
+            # Reshape into pairs, shuffle pairs, then flatten back
+            pairs_c = choices_c[s].reshape(-1, 2)  # Shape: (N/B, 2)
+            np.random.shuffle(pairs_c)  # Shuffle along first dimension
+            choices_c[s] = pairs_c.flatten()  # Back to 1D array
+        choices_c1 = choices_c[:,0::2]
+        choices_c2 = choices_c[:,1::2]
+        
+        # Select OOD target from distribution 2
+        targets_c_ind = np.random.choice(choices_c2.shape[1], size = (S,))
+        targets_c = choices_c2[np.arange(S),targets_c_ind]
+        
+        # Determine if sequence is bursty and/or OOD [filt_B] means non-bursty,[~filt_B] means bursty, [filt_C] means in-distribution,[~filt_C] means ood
+        filt_B = np.random.uniform(size = S) > p_B
+        choices_2[filt_B] = np.random.choice(K2,size  = (np.sum(filt_B),N), p = P2)
+        choices_1[filt_B] = np.random.choice(K1,size  = (np.sum(filt_B),N), p = P1)
+        targets[filt_B] = np.random.choice(K2,size  = (np.sum(filt_B),), p = P2)
+        
+        filt_C = np.random.uniform(size = S) > p_C
+
+        # Context item embeddings sampled from distribution 1
+        e_fac1 = 1/np.sqrt(1+eps1**2)
+        e_fac2 = 1/np.sqrt(1+eps2**2)
+        inputs_mm[filt_C,:-1:3,:] = (e_fac1*(mus_class_m1[choices_1] + eps1*np.random.normal(size = (S,N,D1))/np.sqrt(D1)))[filt_C]
+        inputs_2[filt_C,:-1,:] = (e_fac2*(mus_class_m2[choices_2] + eps2*np.random.normal(size = (S,N,D2))/np.sqrt(D2)))[filt_C]
+        # Label item embeddings sampled from distribution 2
+        if flip_labels:
+            wrong_label = (labels_class_m2 + 1)%L2
+            inputs_mm[filt_C,2:-1:3,:] = mus_label_m2[wrong_label][choices_2][filt_C]
+        else:
+            inputs_mm[filt_C,2:-1:3,:] = mus_label_m2[labels_class_m2][choices_2][filt_C]
+        # Target item embeddings sampled from distribution 2
+        inputs_2[filt_C,-1,:] = (e_fac2*(mus_class_m2[targets] + eps2*np.random.normal(size = (S,D2))/np.sqrt(D2)))[filt_C]
+        
+        # OOD context item embeddings sampled from distribution 1
+        inputs_mm[~filt_C,:-1:3,:] = (e_fac1*(mus_class_new1[choices_c1] + eps1*np.random.normal(size = (S,N,D1))/np.sqrt(D1)))[~filt_C]
+        inputs_2[~filt_C,:-1,:] = (e_fac2*(mus_class_new2[choices_c2] + eps2*np.random.normal(size = (S,N,D2))/np.sqrt(D2)))[~filt_C]
+        # OOD label item embeddings sampled from distribution 2
+        inputs_mm[~filt_C,2:-1:3,:] = mus_label_m2[labels_class_new2][choices_c2][~filt_C]
+        # OOD target item embeddings sampled from distribution 2
+        inputs_2[~filt_C,-1,:] = (e_fac2*(mus_class_new2[targets_c] + eps2*np.random.normal(size = (S,D2))/np.sqrt(D2)))[~filt_C]
+        
+        labels = np.zeros((S,L1),dtype= bool)
+        target_classes = np.zeros(S, dtype = int)
+        label_sequences = np.zeros((S, N+1), dtype=int)
+        
+        for s in range(S):
+            if filt_C[s]:
+                labels[s,labels_class_m2[targets[s]]] = True
+                target_classes[s] = targets[s]
+                label_sequences[s] = np.append(labels_class_m2[choices_2[s]],labels_class_m2[targets[s]])
+            else:
+                labels[s,labels_class_new2[targets_c[s]]] = True
+                target_classes[s] = -1
+                label_sequences[s] = np.append(labels_class_new2[choices_c2[s]],labels_class_new2[targets_c[s]])
+        if seq_labels:
+            return torch.FloatTensor(inputs_mm), torch.FloatTensor(inputs_2), torch.FloatTensor(labels), torch.FloatTensor(label_sequences)
+        elif output_target_labels:
+            return torch.FloatTensor(inputs_mm), torch.FloatTensor(inputs_2), torch.FloatTensor(labels), torch.LongTensor(target_classes)
+        else:
+            return torch.FloatTensor(inputs_mm), torch.FloatTensor(inputs_2), torch.FloatTensor(labels)
+        
+    
+    def __iter__(self):
+        while self.n_samples < self.datasize:
+            inputs_mm, inputs_2, labels = self.generate_sequence()
+            # Convert to tensors and yield as a batch
+            yield inputs_mm, inputs_2, labels
             self.n_samples += 1
--- main_mm_test.py
+++ main_mm_test.py
@@ -5,10 +5,11 @@ import wandb
 import torch
 import numpy as np
 from model import ModelArgs, Transformer, MMTransformer
-from dataset import ICLDataset, get_mus_label_class, generate_input_seqs, generate_input_seqs_mm_v1,get_mm_mus_label_class
+from dataset import ICLDataset, MMDataset, get_mus_label_class, generate_input_seqs, generate_input_seqs_mm_v1,get_mm_mus_label_class
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from tqdm import tqdm
+from torch.utils.data import DataLoader
 WANDB = False
 
 def accuracy(outputs, labels, flip_labels=False, L2=None):
@@ -20,11 +21,11 @@ def accuracy(outputs, labels, flip_labels=False, L2=None):
     return (predictions_inds == labels_inds).float().mean()
 
 
-def evaluate(model, data, flip_labels=False, device=None,L2=None):
+def evaluate(model, dataloader, flip_labels=False, device=None,L2=None):
     model.eval()
     loss_criterion = torch.nn.CrossEntropyLoss()
     with torch.no_grad():
-        inputs_mm, inputs_2, labels = data
+        inputs_mm, inputs_2, labels = dataloader
         inputs_mm = inputs_mm.to(device)
         inputs_2 = inputs_2.to(device)
         labels = labels.to(device)
@@ -39,16 +40,15 @@ def evaluate(model, data, flip_labels=False, device=None,L2=None):
         
         return loss, acc
 
-def train(model,batch_size,mus_label_m1, mus_class_m1, mus_label_m2, mus_class_m2, labels_class_m2, mapping_m2_to_m1,N,eps1,eps2,P1,P2,B,p_B,p_C,test_data, test_ic_data, test_ic2_data, test_iw_data, optimizer, device, print_every, ckpt_store_freq, prefix, niters, n_epochs):
+def train(model,train_loader, test_loader,  test_ic_loader, test_ic2_loader, test_iw_loader,optimizer, device, print_every, ckpt_store_freq, prefix, niters, n_epochs):
     model.to(device)
     loss_criterion = torch.nn.CrossEntropyLoss()
-    # for n, batch in tqdm(enumerate(train_loader), total=niters):
     global_iter = 0
     for epoch in range(n_epochs):
         print(f"Epoch {epoch}")
-        for n in tqdm(range(niters)):
-            inputs_mm, inputs_2, labels = generate_input_seqs_mm_v1(mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1, N=N, S=batch_size, eps1=eps1, eps2=eps2, P1=P1, P2=P2, B=B, p_B=p_B, p_C=p_C)
+        for n, batch in tqdm(enumerate(train_loader), total=niters):
             model.train()
+            inputs_mm, inputs_2, labels = batch
             inputs_mm = inputs_mm.to(device)
             inputs_2 = inputs_2.to(device)
             labels = labels.to(device)
@@ -74,10 +74,10 @@ def train(model,batch_size,mus_label_m1, mus_class_m1, mus_label_m2, mus_class_m
                 
             # Evaluate  
             if global_iter%print_every==0:
-                loss_test, acc_test = evaluate(model, test_data, flip_labels=False, device=device)
-                loss_ic, acc_ic = evaluate(model, test_ic_data, flip_labels=False, device=device)
-                loss_ic2, acc_ic2 = evaluate(model, test_ic2_data, flip_labels=True, device=device,L2=L2)
-                loss_iw, acc_iw = evaluate(model, test_iw_data, flip_labels=False, device=device)
+                loss_test, acc_test = evaluate(model, test_loader, flip_labels=False, device=device)
+                loss_ic, acc_ic = evaluate(model, test_ic_loader, flip_labels=False, device=device)
+                loss_ic2, acc_ic2 = evaluate(model, test_ic2_loader, flip_labels=True, device=device,L2=L2)
+                loss_iw, acc_iw = evaluate(model, test_iw_loader, flip_labels=False, device=device)
                 print(f"Epoch {epoch}, Iteration {n}: Train loss: {loss:.4f}, Train acc: {acc:.4f}, Test loss: {loss_test:.4f}, Test acc: {acc_test:.4f}, IC loss: {loss_ic:.4f}, IC acc: {acc_ic:.4f}, IC2 loss: {loss_ic2:.4f}, IC2 acc: {acc_ic2:.4f}, IW loss: {loss_iw:.4f}, IW acc: {acc_iw:.4f}")
                 if WANDB:
                     wandb.log({"Epoch": epoch, "Iteration": n, "global_iter": global_iter, "Train_Loss": loss, "Train_Accuracy": acc, "Test_Loss": loss_test, "Test_Accuracy": acc_test, "IC_Loss": loss_ic, "IC_Accuracy": acc_ic, "IC2_Loss": loss_ic2, "IC2_Accuracy": acc_ic2, "IW_Loss": loss_iw, "IW_Accuracy": acc_iw})
@@ -86,29 +86,28 @@ def train(model,batch_size,mus_label_m1, mus_class_m1, mus_label_m2, mus_class_m
         
 if __name__ == "__main__":
     # Set up CUDA and random seeds
-    device = torch.device(f"cuda:{int(sys.argv[18])}" if torch.cuda.is_available() else "cpu")
-    SEED = int(sys.argv[18])
+    device = torch.device(f"cuda:{int(sys.argv[25])}" if torch.cuda.is_available() else "cpu")
+    SEED = int(sys.argv[25])
     torch.manual_seed(SEED)
     np.random.seed(SEED)
     torch.backends.cudnn.deterministic = True
 
     # Data Parameters
-    # K1 = K2 = int(sys.argv[1])  # Number of classes
-    K1 = 8192
-    K2 = 1024
-    N = int(sys.argv[2])  # Number of item-label pairs in the context
-    # D1 = D2 = int(sys.argv[3])  # Feature dimension
-    D1 = 64
-    D2 = 32
-    # L1 = L2 = int(sys.argv[4])  # Number of labels
-    L1 = 32
-    L2=16
-    alpha1= alpha2 = float(sys.argv[5])  # Zipf's law exponent
-    B = int(sys.argv[6])  # Burstiness
-    p_B = float(sys.argv[7])  # Fraction of bursty sequences
-    p_C = float(sys.argv[8])  # Fraction of OOD sequences
-    eps1= eps2 = float(sys.argv[9])  # Within-class variance
-    no_repeats = bool(int(sys.argv[10]))  # Whether repeated items are allowed in the context
+    K1 = int(sys.argv[1])
+    K2 = int(sys.argv[2])
+    N = int(sys.argv[3])  # Number of item-label pairs in the context
+    D1 = int(sys.argv[4])
+    D2 = int(sys.argv[5])
+    L1 = int(sys.argv[6])
+    L2 = int(sys.argv[7])
+    alpha1 = float(sys.argv[8])
+    alpha2 = float(sys.argv[9])  # Zipf's law exponent
+    B = int(sys.argv[10])  # Burstiness
+    p_B = float(sys.argv[11])  # Fraction of bursty sequences
+    p_C = float(sys.argv[12])  # Fraction of OOD sequences
+    eps1 = float(sys.argv[13])
+    eps2 = float(sys.argv[14])  # Within-class variance
+    no_repeats = bool(int(sys.argv[15]))  # Whether repeated items are allowed in the context
     
     S = 10000  # Number of sequences in the test set
     Nmax = 32  # Maximum number of item-label pairs in the context
@@ -118,20 +117,21 @@ if __name__ == "__main__":
     P2 /= np.sum(P2)  # Normalized power law distribution
     
     # Model Parameters
-    n_heads = int(sys.argv[11])  # Number of attention heads
-    n_layers = int(sys.argv[12])  # Number of transformer layers
-    rope = bool(int(sys.argv[13]))  # Whether to use RoPE
-    rope_theta = int(sys.argv[14])  # Rope base
-    rms_norm = bool(int(sys.argv[15])) # Whether to use RMS normalization
-    L_pos = int(sys.argv[16])
+    n_heads = int(sys.argv[16])  # Number of attention heads
+    n_layers = int(sys.argv[17])  # Number of transformer layers
+    rope = bool(int(sys.argv[18]))  # Whether to use RoPE
+    rope_theta = int(sys.argv[19])  # Rope base
+    rms_norm = bool(int(sys.argv[20])) # Whether to use RMS normalization
+    L_pos = int(sys.argv[21])
+    freeze_layers = bool(int(sys.argv[22]))
     
     # Training parameters
-    niters = 30000  # Number of iterations
+    niters = 150000  # Number of iterations
     n_epochs = 1  # Number of epochs
-    batch_size = int(sys.argv[16])
+    batch_size = int(sys.argv[23])
     lr = 1e-3  # Learning rate
     weight_decay = 1e-6  # Weight decay
-    optimizer = sys.argv[17]
+    optimizer = sys.argv[24]
     print_every = 100  # Print every n iterations
     ckpt_store_freq = 10000 # Store every n iterations
     
@@ -142,36 +142,42 @@ if __name__ == "__main__":
         input_dim = L_pos + D1
         
    # Initialize wandb
-    prefix = f"./outs_torch/K1{K1}_K2{K2}_N{N}_D1{D1}_D2{D2}_L1{L1}_L2{L2}_alpha1{alpha1}_alpha2{alpha2}_B{B}_pB{p_B}_pC{p_C}_eps1{eps1}_eps2{eps2}_no_repeats{no_repeats}_rope_theta{rope_theta}_n_heads{n_heads}_n_layers{n_layers}_rms_norm{rms_norm}_optimizer{optimizer}_niters{niters}_n_epochs{n_epochs}"
-    # if WANDB:
-    #     wandb.init(project="ICL_torch",
-    #             name=f"run_{SEED}_{prefix.split('/')[-1]}",
-    #             config={
-    #                 "K": K,
-    #                 "N": N,
-    #                 "D": D,
-    #                 "L": L,
-    #                 "S": S,
-    #                 "Nmax": Nmax,
-    #                 "alpha": alpha,
-    #                 "B": B,
-    #                 "pB": p_B,
-    #                 "pC": p_C,
-    #                 "eps": eps,
-    #                 "no_repeats": no_repeats,
-    #                 "rope": rope,
-    #                 "rope_theta": rope_theta,
-    #                 "n_heads": n_heads,
-    #                 "n_layers": n_layers,
-    #                 "rms_norm": rms_norm,
-    #                 "niters": niters,
-    #                 "batch_size": batch_size,
-    #                 "lr": lr,
-    #                 "weight_decay": weight_decay,
-    #                 "optimizer": optimizer,
-    #                 "seed": SEED,
-    #                 "n_epochs": n_epochs
-    #             })
+    prefix = f"./outs_torch/K1_{K1}_K2_{K2}_N{N}_D1_{D1}_D2_{D2}_L1_{L1}_L2_{L2}_alpha1_{alpha1}_alpha2_{alpha2}_B{B}_pB{p_B}_pC{p_C}_eps1_{eps1}_eps2_{eps2}_no_repeats{no_repeats}_rope_{rope}_rope_theta{rope_theta}_n_heads{n_heads}_n_layers{n_layers}_rms_norm{rms_norm}_optimizer{optimizer}_niters{niters}_n_epochs{n_epochs}"
+    if WANDB:
+        wandb.init(project="ICL_torch",
+                name=f"run_{SEED}_{prefix.split('/')[-1]}",
+                config={
+                    "K1": K1,
+                    "K2": K2,
+                    "N": N,
+                    "D1": D1,
+                    "D2": D2,
+                    "L1": L1,
+                    "L2": L2,
+                    "S": S,
+                    "Nmax": Nmax,
+                    "alpha1": alpha1,
+                    "alpha2": alpha2,
+                    "B": B,
+                    "pB": p_B,
+                    "pC": p_C,
+                    "eps1": eps1,
+                    "eps2": eps2,
+                    "no_repeats": no_repeats,
+                    "rope": rope,
+                    "rope_theta": rope_theta,
+                    "n_heads": n_heads,
+                    "n_layers": n_layers,
+                    "rms_norm": rms_norm,
+                    "niters": niters,
+                    "batch_size": batch_size,
+                    "lr": lr,
+                    "weight_decay": weight_decay,
+                    "optimizer": optimizer,
+                    "seed": SEED,
+                    "n_epochs": n_epochs,
+                    "L_pos": L_pos
+                })
 
     # Initialize model
     model_args = ModelArgs(
@@ -193,10 +199,10 @@ if __name__ == "__main__":
     print(model)
     weights = "/shared-local/aoq609/MLLM_ICL/ICL/outs_torch/K8192_N8_D64_alpha0.0_B2_pB1.0_pC0.0_eps0.1_no_repeatsFalse_rope_theta10000_n_heads1_n_layers2_rms_normTrue_optimizerSGD_niters30000_n_epochs10/seed_0/ckpt_150000.pt"
     model.load_state_dict(torch.load(weights),strict=False)
-    # Freezde the layers
-    # for p in model.layers.parameters():
-    #     p.requires_grad = False
-    # Initialize optimizer
+    if freeze_layers:
+        for p in model.layers.parameters():
+            p.requires_grad = False
+    #Initialize optimizer
     if optimizer == "SGD":
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay)
     elif optimizer == "Adam":
@@ -206,11 +212,18 @@ if __name__ == "__main__":
 
     # Initialize datasets
     mus_label_m1, mus_class_m1, labels_class_m1, mus_label_m2, mus_class_m2, labels_class_m2, mapping_m2_to_m1 = get_mm_mus_label_class(K1=K1,K2=K2,L1=L1,L2=L2,D1=D1,D2=D2)
+    train_dataset = MMDataset(mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1, N=N,S=batch_size,eps1=eps1,eps2=eps2, P1 = P1, P2 = P2, B=B, p_B = p_B, p_C = p_C, no_repeats = no_repeats, datasize=niters)
+    train_loader = DataLoader(train_dataset, batch_size=None,num_workers=1)
     print("Generating test data...")
     test_data  = generate_input_seqs_mm_v1(mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1, N=N,S=S,eps1=eps1,eps2=eps2, P1 = P1, P2 = P2, B=B, p_B = p_B, p_C = p_C, no_repeats = no_repeats)
     test_ic_data = generate_input_seqs_mm_v1(mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1, N=N,S=S,eps1=eps1,eps2=eps2, P1 = P1, P2 = P2, B = B, p_B = 1, p_C = 1, no_repeats = no_repeats)
     test_ic2_data = generate_input_seqs_mm_v1(mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1, N=N,S=S,eps1=eps1,eps2=eps2, P1 = P1, P2 = P2, B = B, p_B = 1, p_C = 0, flip_labels = True, no_repeats = no_repeats)
     test_iw_data = generate_input_seqs_mm_v1(mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1, N=N,S=S,eps1=eps1,eps2=eps2, P1 = P1, P2 = P2, B = 0, p_B = 0, p_C = 0, no_repeats = no_repeats)
+    test_loader = DataLoader(test_data, batch_size=None,num_workers=1)
+    test_ic_loader = DataLoader(test_ic_data, batch_size=None,num_workers=1)
+    test_ic2_loader = DataLoader(test_ic2_data, batch_size=None,num_workers=1)
+    test_iw_loader = DataLoader(test_iw_data, batch_size=None,num_workers=1)
+    
     print("Training model...")
-    train(model=model, batch_size=batch_size,mus_label_m1=mus_label_m1, mus_class_m1=mus_class_m1, mus_label_m2=mus_label_m2, mus_class_m2=mus_class_m2, labels_class_m2=labels_class_m2, mapping_m2_to_m1=mapping_m2_to_m1,N=N,eps1=eps1,eps2=eps2,B=B,p_B=p_B,P1=P1,P2=P2,p_C=p_C,test_data=test_data, test_ic_data=test_ic_data, test_ic2_data=test_ic2_data, test_iw_data=test_iw_data, optimizer=optimizer, device=device, print_every=print_every, ckpt_store_freq=ckpt_store_freq, prefix=prefix, niters=niters, n_epochs=n_epochs)
+    train(model=model, train_loader=train_loader, test_loader=test_loader,  test_ic_loader=test_ic_loader, test_ic2_loader=test_ic2_loader, test_iw_loader=test_iw_loader, optimizer=optimizer, device=device, print_every=print_every, ckpt_store_freq=ckpt_store_freq, prefix=prefix, niters=niters, n_epochs=n_epochs)
     
